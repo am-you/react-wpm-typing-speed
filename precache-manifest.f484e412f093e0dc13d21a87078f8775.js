64fe1766897c6c986812a6eb89ac85931f83efdf
amyou <amine.youssfi10@gmail.com>
2020-06-21 19:48:42 +0200

    Updates

--- precache-manifest.f484e412f093e0dc13d21a87078f8775.js
+++ precache-manifest.f484e412f093e0dc13d21a87078f8775.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4f6c5c8a030ac9bdaa4b81c78da3879",
+    "revision": "07aab25763bbf935ffe54179e7b727d8",
     "url": "/react-wpm-typing-speed/index.html"
   },
   {
-    "revision": "53c61f9384f3124a7305",
+    "revision": "8fcfa8cd5765631291e1",
     "url": "/react-wpm-typing-speed/static/css/main.7bd5103b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-wpm-typing-speed/static/js/2.1454823c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "53c61f9384f3124a7305",
-    "url": "/react-wpm-typing-speed/static/js/main.5bd8a224.chunk.js"
+    "revision": "8fcfa8cd5765631291e1",
+    "url": "/react-wpm-typing-speed/static/js/main.3f59c5a0.chunk.js"
   },
   {
     "revision": "399c9681380447ff7871",
